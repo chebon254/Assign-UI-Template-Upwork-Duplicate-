2b15703adfe5249e4aff35f05eaac668b493d63f
kelvin254chebonkelvin <kelvinchebon90@gmail.com>
2021-07-10 22:56:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("testimonial");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  // /* == For dots == */
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("testimonial");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) 
+  {slideIndex = 1
+  }
+  slides[slideIndex-1].style.display = "block";
+
+  setTimeout(showSlides, 6000); // Change image every 6 seconds
+}
+
+/* == Change Page On Click == */
+function jobs() {
+  location.href="jobPost.html"
+}
